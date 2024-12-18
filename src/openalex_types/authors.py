8ed0f6eb09e0ca49ae9100993493282ed3ad362b
Antoine Gerardin <antoine.gerardin@nextonicsolutions.com>
2024-12-18 12:38:33 +0000

    fix: authors id are not optional.

--- src/openalex_types/authors.py
+++ src/openalex_types/authors.py
@@ -19,7 +19,7 @@ from pydantic import BaseModel, model_validator, field_validator
 
 class AuthorCountByYear(BaseCountByYear, SQLTable):
     """Author Count by Year."""
-    author_id: str
+    author_id: Optional[str] = None
     _sql_table_name = "openalex.authors_counts_by_year"
     _sql_order = ["author_id", "year", "works_count",
                   "cited_by_count", "oa_works_count"]
